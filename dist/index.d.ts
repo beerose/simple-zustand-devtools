9b9310eb8d5691417fd7730a92f694eeae1e4d4a
beerose <alexsandra.sikora@gmail.com>
2022-06-18 18:49:42 +0200

    Remove dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { StoreApi } from 'zustand';
-declare type ZustandStore = StoreApi<Record<string | number | symbol, any>>;
-export declare function mountStoreDevtool(storeName: string, store: ZustandStore, rootElement?: HTMLElement): void;
-export {};
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-         'use strict'
-
-      if (process.env.NODE_ENV === 'production') {
-        module.exports = require('./simple-zustand-devtools.cjs.production.js')
-      } else {
-        module.exports = require('./simple-zustand-devtools.cjs.development.js')
-      }
